58db2c585810662808e4acdc81e4b5e0cc975ce0
ibrahim eryılmaz <veteryilmaz@gmail.com>
2022-04-06 20:31:07 +0300

    Initial Commit

---
+++ PointTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PointTable
+//
+//  Created by Narkoz on 6.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
